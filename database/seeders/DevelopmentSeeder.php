5c4ccd45e8d9fb97c28298babd6ae091ac8b2b46
adnan-zahir <adnanzaidanz@gmail.com>
2024-12-09 15:01:23 +0700

    feat: added some development seeder - UomSeeder - FcrSeeder - ProductSeeder  - ProductCategory  - ProductSubCategory
    `php artisan db:seed --class=DevelopmentSeeder` will run DatabaseSeeder plus all the above.

---
+++ database/seeders/DevelopmentSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DevelopmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->call([
+            DatabaseSeeder::class,
+            UomSeeder::class,
+            ProductSeeder::class,
+            FcrSeeder::class,
+        ]);
+    }
+}
