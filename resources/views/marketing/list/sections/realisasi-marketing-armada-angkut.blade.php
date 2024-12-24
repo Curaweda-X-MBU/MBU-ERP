7a452c0c1712eb720eee9bfc53b561105757731b
adnan-zahir <adnanzaidanz@gmail.com>
2024-12-25 01:24:28 +0700

    feat: marketing.list.realization limit delivery qty

--- resources/views/marketing/list/sections/realisasi-marketing-armada-angkut.blade.php
+++ resources/views/marketing/list/sections/realisasi-marketing-armada-angkut.blade.php
@@ -31,13 +31,16 @@
                 <td class="py-2">
                     <input name="plat_number" type="text" class="form-control" placeholder="No Polisi" required>
                 </td>
-                <td class="py-2">
+                <td class="py-2 position-relative">
                     <select name="marketing_product_id" class="form-control vehicle_product_select" required>
                         <option value="">Pilih Produk</option>
                     </select>
+                    <small class="form-text text-muted text-right position-absolute pr-1" style="right: 0; font-size: 80%;">Sisa :<span class="current_stock">0</span></small>
                 </td>
-                <td class="py-2">
-                    <input name="qty" type="text" class="form-control numeral-mask" placeholder="Jumlah" required>
+                <td class="py-2 position-relative">
+                    <input type="number" name="qty" id="qty" max="0" class="position-absolute" style="opacity: 0; pointer-events: none;" tabindex="-1">
+                    <input type="text" class="form-control numeral-mask qty_mask" placeholder="Jumlah" required>
+                    <span class="text-danger text-right small position-absolute pr-1 invalid" style="right: 0; font-size: 80%; opacity: 0;">Melebihi stock</span>
                 </td>
                 <td class="py-2">
                     <input name="uom_id" type="hidden">
@@ -70,6 +73,58 @@
 
 <script>
     $(function() {
+        function setField($this) {
+            const data = $this.select2('data')[0];
+            const uom_id = data.element.dataset.uom_id;
+            const uom_name = data.element.dataset.uom_name;
+            const qty = data.element.dataset.qty;
+
+            const $rowScope = $this.closest('tr');
+
+            const $uomId = $rowScope.find('input[name*="uom_id"]');
+            const $uomName = $rowScope.find('input[name*="uom_name"]');
+            const $currentStock = $rowScope.find('.current_stock');
+            const $qty = $rowScope.find('input[name*="qty"]');
+
+            $uomId.val(uom_id);
+            $uomName.val(uom_name);
+            $qty.attr('max', qty);
+            $currentStock.text(parseNumToLocale(parseInt(qty - $qty.val())).split(',')[0]);
+
+        }
+
+        function updateQty($this) {
+            const $rowScope = $this.closest('tr');
+            const $qty = $rowScope.find('input[name*="qty"]');
+            const max = $qty.attr('max');
+
+            const val = parseLocaleToNum($this.val());
+
+            $qty.val(val);
+
+            // cross check
+            const $currentProduct = $rowScope.find('select[name*="marketing_product_id"]');
+            const currentProductId = $currentProduct.val();
+            const $products = $('#marketing-delivery-vehicles-repeater-1 select[name*="marketing_product_id"]');
+            const $filteredProducts = $products.filter(function() {
+                return $(this).val() === currentProductId;
+            }).filter(function() {
+                return $(this).data('select2Id') !== $currentProduct.data('select2Id');
+            });
+            const otherQty = $filteredProducts.map(function() {
+                return $(this).closest('tr').find('input[name*="qty"]').val();
+            }).get().reduce((cur, arr) => cur + parseInt(arr), 0);
+
+            const currentStock = parseNumToLocale(parseInt(max - val - otherQty)).split(',')[0];
+
+            $rowScope.find('.current_stock').text(currentStock);
+            $filteredProducts.map(function() {
+                return $(this).closest('tr').find('.current_stock');
+            }).each(function() {
+                $(this).text(currentStock);
+            });
+        }
+
         const dateOpt = { dateFormat: 'd-M-Y' };
         const dateTimeOpt = { dateFormat: 'd-M-Y H:i', enableTime: true };
         const optMarketingDeliveryVehicles = {
@@ -86,18 +141,23 @@
                 if ('{{ @$data->marketing_products }}') {
                     const marketingProducts = @json($data->marketing_products);
                     marketingProducts.forEach((marketingProduct, i) => {
-                        $productSelect.append(`<option data-uom_id="${marketingProduct.uom.uom_id}" data-uom_name="${marketingProduct.uom.name}" value="${marketingProduct.marketing_product_id}">${marketingProduct.product.name}</option>`)
+                        $productSelect.append(`<option data-qty="${marketingProduct.qty}" data-uom_id="${marketingProduct.uom.uom_id}" data-uom_name="${marketingProduct.uom.name}" value="${marketingProduct.marketing_product_id}">${marketingProduct.product.name}</option>`)
                     });
                 }
 
-                const $uomId = $row.find('input[name*="uom_id"]');
-                const $uomName = $row.find('input[name*="uom_name"]');
                 $productSelect.on('select2:select', function() {
-                    const data = $(this).select2('data')[0];
-                    const uom_id = data.element.dataset.uom_id;
-                    const uom_name = data.element.dataset.uom_name;
-                    $uomId.val(uom_id);
-                    $uomName.val(uom_name);
+                    setField($(this));
+                });
+
+                $row.find('.qty_mask').on('input', function() {
+                    updateQty($(this));
+                    const $stock = $row.find('.current_stock');
+                    const stock = parseLocaleToNum($stock.text());
+                    if (stock < 0) {
+                        $(this).siblings('.invalid').css('opacity', 1);
+                    } else {
+                        $(this).siblings('.invalid').css('opacity', 0);
+                    }
                 });
 
                 initNumeralMask('.numeral-mask');
