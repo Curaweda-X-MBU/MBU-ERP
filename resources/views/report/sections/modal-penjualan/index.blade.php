d4ae85b432618dc2cb24d7705791f0b7047069ba
Alifiasraa <ajeng1109@gmail.com>
2025-01-20 15:42:31 +0700

    feat: add pop-up modal for no.dtps penjualan

--- resources/views/report/sections/modal-penjualan/index.blade.php
+++ resources/views/report/sections/modal-penjualan/index.blade.php
@@ -1,18 +1,18 @@
-<!-- Modal -->
-<div class="modal fade" id="penjualanModal" tabindex="-1" aria-labelledby="penjualanModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg modal-dialog-centered">
+<div class="modal fade" id="penjualanModal" tabindex="-1">
+    <div class="modal-dialog modal-xl modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header p-2">
                 <h5 class="modal-title text-primary" id="penjualanModalLabel" style="font-size: 1.5rem">
-                    Detail Penjualan Ayam Besar | DO.MBU.00913
+                    Detail Penjualan Ayam Besar | DO.MBU.00912
                 </h5>
                 <button type="button" class="close mr-0" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
+                @include('report.sections.modal-penjualan.produk-penjualan-collapse')
+                @include('report.sections.modal-penjualan.biaya-lainnya-collapse')
             </div>
         </div>
     </div>
 </div>
-
